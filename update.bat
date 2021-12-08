bcb520439fe3d89c9eab4668f5e182d7500af29a
id7mgh <68156126+id7mgh@users.noreply.github.com>
2021-12-08 20:57:02 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
-set "upver=v0.8"
-set "upverlink=https://github.com/id7mgh/id7mghTool/releases/download/v0.8/id7mgh.Tool.v0.8.bat"
+set "upver=v0.8.1"
+set "upverlink=https://github.com/id7mgh/id7mghTool/releases/download/v0.8.1/id7mgh_Tool_v0.8.1.bat"
